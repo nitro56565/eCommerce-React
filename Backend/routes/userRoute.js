f81fa12cab46775f60fac21d45f503b853c319f5
Rutuja-28 <rutujahursale28@gmail.com>
2025-03-07 11:58:54 +0530

    replaced got package with axios
    Signed-off-by: Rutuja-28 <rutujahursale28@gmail.com>

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { dashboard, login, register } from '../controller/userController.js';
 import {createOrder, verifyPayment} from '../controller/paymentsController.js';
-import { createOrder, captureOrder } from '../controller/paypalPaymentController.js';
+import { createOrderPaypal, captureOrder } from '../controller/paypalPaymentController.js';
 
 const route = express.Router();
 route.post('/createOrder', createOrder);
@@ -9,7 +9,7 @@ route.post('/verifyPayment', verifyPayment);
 route.post("/register", register)
 route.post('/login', login)
 route.get('/dashboard', dashboard)
-route.post('/create-order',createOrder)
+route.post('/create-order',createOrderPaypal)
 route.get('/capture-order/:paymentId', captureOrder)
 
 export default route;
